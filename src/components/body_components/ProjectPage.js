329a40e0751afffae734f54255a2fca8c806d598
Jaime Bisuna <jaimebisona@gmail.com>
2020-06-28 18:05:51 +0800

    Added apps video demo

--- src/components/body_components/ProjectPage.js
+++ src/components/body_components/ProjectPage.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Flickity from 'react-flickity-component'
+import ReactPlayer from 'react-player'
 import {
     useParams,
-    useHistory
+    useHistory,
+    Link
 } from "react-router-dom";
 import '../../assets/css/flickity.css';
 import '../../assets/css/fullscreen.css';
@@ -11,7 +13,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
+
 function ProjectPage(props) {
+    const [showDemo, setShowDemo] = useState(false)
     let { id } = useParams()
     let { title, description, urls, images } = props.data[id]
     let history = useHistory()
@@ -26,9 +30,27 @@ function ProjectPage(props) {
         imagesLoaded: true,
         lazyLoad: true
     }
-    return (
-        <section className="showcase">
-            <Flickity
+    useEffect(() => {
+        console.log('used effect!')
+    })
+
+    const handleClick = () => {
+        console.log('scroll handled')
+        setShowDemo(!showDemo);
+        console.log(showDemo)
+    }
+
+    const renderButton = (id) => {
+        if (!showDemo) {
+            return <button id='demo_btn' onClick={handleClick} className={`${id}`}>Play Demo</button>
+        } else {
+            return <button id='demo_btn' onClick={handleClick} className={`${id}`}>Close Demo</button>
+        }
+    }
+
+    const renderVideo = () => {
+        if (!showDemo) {
+            return <Flickity
                 className={"showcase_left_grid " + id}
                 elementType="div"
                 options={sliderOptions}
@@ -37,15 +59,27 @@ function ProjectPage(props) {
             >
                 {images.map((elem, i) => <img key={i} src={require(`../../assets/images/${id}/${elem}`)} data-flickity-lazyload={require(`../../assets/images/${id}/${elem}`)} alt="Image_Here" ></img>)}
             </Flickity>
+        } else {
+            return <div className={`showcase_left_grid ${id}`}>
+                <ReactPlayer url={require(`../../assets/demo/${id}_demo.mp4`)} width='100%' height='100%' controls={true} />
+            </div>
+        }
+    }
+
+
+    return (
+        <section className="showcase">
+            {renderVideo()}
             <div className="showcase_right_grid">
                 <div className="showcase_title">
-                    <a onClick={history.goBack} className={id} >
+                    <Link to='/' className={id}>
                         <FontAwesomeIcon icon={faArrowLeft} size="1x" />
-                    </a>
+                    </Link>
                     <h1>{title}</h1>
                 </div>
                 <div className="showcase_details">
                     {description}
+                    {renderButton(id)}
                     <div className="links">
                         <a href={urls['github']} className="circle">
                             <FontAwesomeIcon icon={faGithub} size="1x" color="white" />
