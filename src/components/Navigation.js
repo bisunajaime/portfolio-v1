b240b1da13ac121a45d8d96b333bcc944a1d7eb6
camboobabbage <josejaime.bisuna@benilde.edu.ph>
2020-07-23 16:50:19 +0800

    Added colors, design looked dead

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,34 +1,52 @@
-import React from 'react'
-import logo from '../assets/images/logo.svg'
-
+import React from "react";
+import logo from "../assets/images/logo.svg";
 
 function Navigation() {
+  function toggleNav() {
+    let nav = document.getElementById("navigation");
+    let main = document.getElementById("main");
+    main.classList.toggle("closed");
+    nav.classList.toggle("closed");
+  }
 
-    function toggleNav() {
-        let nav = document.getElementById('navigation')
-        let main = document.getElementById('main')
-        main.classList.toggle("closed")
-        nav.classList.toggle("closed")
-    }
-
-    return (
-        <section className="navigation closed" id="navigation">
-            <ul>
-                <li><a href="#aboutme" onClick={toggleNav}>About</a></li>
-                <li><a href="#techstack" onClick={toggleNav}>Techstack</a></li>
-                <li><a href="#projects" onClick={toggleNav}>Projects</a></li>
-                <li><a href="#events" onClick={toggleNav}>Events</a></li>
-                <li><a href="#organizations" onClick={toggleNav}>Organizations</a></li>
-            </ul>
-            <div className="card">
-                <img src={logo} alt="Logo" />
-                <div className="card_info">
-                    <h4>Jose Jaime Bisuna</h4>
-                    <p>Software Developer</p>
-                </div>
-            </div>
-        </section>
-    )
+  return (
+    <section className="navigation closed" id="navigation">
+      <ul>
+        <li>
+          <a href="#aboutme" onClick={toggleNav} className="selected">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="#techstack" onClick={toggleNav}>
+            Techstack
+          </a>
+        </li>
+        <li>
+          <a href="#projects" onClick={toggleNav}>
+            Projects
+          </a>
+        </li>
+        <li>
+          <a href="#events" onClick={toggleNav}>
+            Events
+          </a>
+        </li>
+        <li>
+          <a href="#organizations" onClick={toggleNav}>
+            Organizations
+          </a>
+        </li>
+      </ul>
+      <div className="card">
+        <img src={logo} alt="Logo" />
+        <div className="card_info">
+          <h4>Jose Jaime Bisuna</h4>
+          <p>Software Developer</p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Navigation
+export default Navigation;
--- src/components/body_components/TechStack.js
+++ src/components/body_components/TechStack.js
@@ -1,162 +1,254 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faDatabase, } from '@fortawesome/free-solid-svg-icons'
-import firebase from '../../assets/svg/firebase.svg'
-import flutter from '../../assets/svg/flutter.svg'
-import { faJava, faJs, faHtml5, faCss3, faPhp, faReact, faAndroid, faNodeJs, faLaravel, faGithub, faBootstrap, faSass, faWordpressSimple, } from '@fortawesome/free-brands-svg-icons'
-import Fade from 'react-reveal/Fade'
-
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDatabase } from "@fortawesome/free-solid-svg-icons";
+import firebase from "../../assets/svg/firebase.svg";
+import flutter from "../../assets/svg/flutter.svg";
+import {
+  faJava,
+  faJs,
+  faHtml5,
+  faCss3,
+  faPhp,
+  faReact,
+  faAndroid,
+  faNodeJs,
+  faLaravel,
+  faGithub,
+  faBootstrap,
+  faSass,
+  faWordpressSimple,
+} from "@fortawesome/free-brands-svg-icons";
+import Fade from "react-reveal/Fade";
 
 export default function TechStack() {
-
-    return (
-        <section className="techstack" id="techstack">
-            <Fade cascade bottom>
-                <div className="header">
-                    <h1><span role="img" aria-label="techstack">💻</span> Tech Stack</h1>
-                    <p>Some things I know that I can use to create projects.</p>\
-                </div>
-            </Fade>
-            <Fade bottom >
-                <div className="techstack_list" id="info" >
-                    <div className="techstack_box blue" id='box'>
-                        <img alt='flutter_logo' src={flutter} className="fa_icon" />
-                        <p>Flutter</p>
-                        <div className='lang_info hide' id='flutter_knowledge'>
-                            <h1>50%</h1>
-                            <p>Flutter</p>
-                        </div>
-                    </div>
-                    <div className="techstack_box orange" >
-                        <img alt='firebase_logo' src={firebase} className="fa_icon" />
-                        <p>Firebase</p>
-                        <div className='lang_info hide' id='flutter_knowledge'>
-                            <h1>50%</h1>
-                            <p>Flutter</p>
-                        </div>
-                    </div>
-                    <div className="techstack_box orange" >
-                        <FontAwesomeIcon icon={faJava} color="orange" fixedWidth className="fa_icon" />
-                        <p>Java</p>
-                        <div className='lang_info hide' id='flutter_knowledge'>
-                            <h1>50%</h1>
-                            <p>Flutter</p>
-                        </div>
-                    </div>
-                    <div className="techstack_box orange" >
-                        <FontAwesomeIcon icon={faHtml5} color="#ff8e25" fixedWidth className="fa_icon" />
-                        <p>HTML</p>
-                        <div className='lang_info hide' id='flutter_knowledge'>
-                            <h1>50%</h1>
-                            <p>Flutter</p>
-                        </div>
-                    </div>
-                    <div className="techstack_box blue" >
-                        <FontAwesomeIcon icon={faCss3} color="lightblue" fixedWidth className="fa_icon" />
-                        <p>CSS</p>
-                        <div className='lang_info hide' id='flutter_knowledge'>
-                            <h1>50%</h1>
-                            <p>Flutter</p>
-                        </div>
-                    </div>
-                    <div className="techstack_box red" >
-                        <FontAwesomeIcon icon={faSass} color="lightpink" fixedWidth className="fa_icon" />
-                        <p>SASS</p>
-                        <div className='lang_info hide' id='flutter_knowledge'>
-                            <h1>50%</h1>
-                            <p>Flutter</p>
-                        </div>
-                    </div>
-                    <div className="techstack_box orange" >
-                        <FontAwesomeIcon icon={faJs} color="orange" fixedWidth className="fa_icon" />
-                        <p>JS</p>
-                        <div className='lang_info hide' id='flutter_knowledge'>
-                            <h1>50%</h1>
-                            <p>Flutter</p>
-                        </div>
-                    </div>
-                    <div className="techstack_box purple" >
-                        <FontAwesomeIcon icon={faBootstrap} color="#dfa8ff" fixedWidth className="fa_icon" />
-                        <p>Bootstrap</p>
-                        <div className='lang_info hide' id='flutter_knowledge'>
-                            <h1>50%</h1>
-                            <p>Flutter</p>
-                        </div>
-                    </div>
-                    <div className="techstack_box green" >
-                        <FontAwesomeIcon icon={faNodeJs} color="lightgreen" fixedWidth className="fa_icon" />
-                        <p>Node</p>
-                        <div className='lang_info hide' id='flutter_knowledge'>
-                            <h1>50%</h1>
-                            <p>Flutter</p>
-                        </div>
-                    </div>
-                    <div className="techstack_box purple" >
-                        <FontAwesomeIcon icon={faPhp} color="#dfa8ff" fixedWidth className="fa_icon" />
-                        <p>PHP</p>
-                        <div className='lang_info hide' id='flutter_knowledge'>
-                            <h1>50%</h1>
-                            <p>Flutter</p>
-                        </div>
-                    </div>
-                    <div className="techstack_box white" >
-                        <FontAwesomeIcon icon={faWordpressSimple} color="white" fixedWidth className="fa_icon" />
-                        <p>Wordpress</p>
-                        <div className='lang_info hide' id='flutter_knowledge'>
-                            <h1>50%</h1>
-                            <p>Flutter</p>
-                        </div>
-                    </div>
-                    <div className="techstack_box red" >
-                        <FontAwesomeIcon icon={faDatabase} color="#ffa9a9" fixedWidth className="fa_icon" />
-                        <p>MS SQL</p>
-                        <div className='lang_info hide' id='flutter_knowledge'>
-                            <h1>50%</h1>
-                            <p>Flutter</p>
-                        </div>
-                    </div>
-                    <div className="techstack_box blue" >
-                        <FontAwesomeIcon icon={faReact} color="#b0e6ff" fixedWidth className="fa_icon" />
-                        <p>React</p>
-                        <div className='lang_info hide' id='flutter_knowledge'>
-                            <h1>50%</h1>
-                            <p>Flutter</p>
-                        </div>
-                    </div>
-                    <div className="techstack_box green" >
-                        <FontAwesomeIcon icon={faAndroid} color="#b8ffbc" fixedWidth className="fa_icon" />
-                        <p>Android</p>
-                        <div className='lang_info hide' id='flutter_knowledge'>
-                            <h1>50%</h1>
-                            <p>Flutter</p>
-                        </div>
-                    </div>
-                    <div className="techstack_box blue" >
-                        <FontAwesomeIcon icon={faDatabase} color="lightblue" fixedWidth className="fa_icon" />
-                        <p>MySQL</p>
-                        <div className='lang_info hide' id='flutter_knowledge'>
-                            <h1>50%</h1>
-                            <p>Flutter</p>
-                        </div>
-                    </div>
-                    <div className="techstack_box red" >
-                        <FontAwesomeIcon icon={faLaravel} color="red" fixedWidth className="fa_icon" />
-                        <p>Laravel</p>
-                        <div className='lang_info hide' id='flutter_knowledge'>
-                            <h1>50%</h1>
-                            <p>Flutter</p>
-                        </div>
-                    </div>
-                    <div className="techstack_box white" >
-                        <FontAwesomeIcon icon={faGithub} color="white" fixedWidth className="fa_icon" />
-                        <p>Github</p>
-                        <div className='lang_info hide' id='flutter_knowledge'>
-                            <h1>50%</h1>
-                            <p>Flutter</p>
-                        </div>
-                    </div>
-                </div>
-            </Fade>
-        </section>
-    )
+  return (
+    <section className="techstack" id="techstack">
+      <Fade cascade bottom>
+        <div className="header">
+          <h1>
+            <span role="img" aria-label="techstack">
+              💻
+            </span>{" "}
+            Tech Stack
+          </h1>
+          <p>Some things I know that I can use to create projects.</p>
+        </div>
+      </Fade>
+      <Fade bottom>
+        <div className="techstack_list" id="info">
+          <div className="techstack_box blue" id="box">
+            <img alt="flutter_logo" src={flutter} className="fa_icon" />
+            <p>Flutter</p>
+            <div className="lang_info hide" id="flutter_knowledge">
+              <h1>50%</h1>
+              <p>Flutter</p>
+            </div>
+          </div>
+          <div className="techstack_box orange">
+            <img alt="firebase_logo" src={firebase} className="fa_icon" />
+            <p>Firebase</p>
+            <div className="lang_info hide" id="flutter_knowledge">
+              <h1>50%</h1>
+              <p>Flutter</p>
+            </div>
+          </div>
+          <div className="techstack_box orange">
+            <FontAwesomeIcon
+              icon={faJava}
+              color="orange"
+              fixedWidth
+              className="fa_icon"
+            />
+            <p>Java</p>
+            <div className="lang_info hide" id="flutter_knowledge">
+              <h1>50%</h1>
+              <p>Flutter</p>
+            </div>
+          </div>
+          <div className="techstack_box orange">
+            <FontAwesomeIcon
+              icon={faHtml5}
+              color="#ff8e25"
+              fixedWidth
+              className="fa_icon"
+            />
+            <p>HTML</p>
+            <div className="lang_info hide" id="flutter_knowledge">
+              <h1>50%</h1>
+              <p>Flutter</p>
+            </div>
+          </div>
+          <div className="techstack_box blue">
+            <FontAwesomeIcon
+              icon={faCss3}
+              color="lightblue"
+              fixedWidth
+              className="fa_icon"
+            />
+            <p>CSS</p>
+            <div className="lang_info hide" id="flutter_knowledge">
+              <h1>50%</h1>
+              <p>Flutter</p>
+            </div>
+          </div>
+          <div className="techstack_box red">
+            <FontAwesomeIcon
+              icon={faSass}
+              color="lightpink"
+              fixedWidth
+              className="fa_icon"
+            />
+            <p>SASS</p>
+            <div className="lang_info hide" id="flutter_knowledge">
+              <h1>50%</h1>
+              <p>Flutter</p>
+            </div>
+          </div>
+          <div className="techstack_box orange">
+            <FontAwesomeIcon
+              icon={faJs}
+              color="orange"
+              fixedWidth
+              className="fa_icon"
+            />
+            <p>JS</p>
+            <div className="lang_info hide" id="flutter_knowledge">
+              <h1>50%</h1>
+              <p>Flutter</p>
+            </div>
+          </div>
+          <div className="techstack_box purple">
+            <FontAwesomeIcon
+              icon={faBootstrap}
+              color="#dfa8ff"
+              fixedWidth
+              className="fa_icon"
+            />
+            <p>Bootstrap</p>
+            <div className="lang_info hide" id="flutter_knowledge">
+              <h1>50%</h1>
+              <p>Flutter</p>
+            </div>
+          </div>
+          <div className="techstack_box green">
+            <FontAwesomeIcon
+              icon={faNodeJs}
+              color="lightgreen"
+              fixedWidth
+              className="fa_icon"
+            />
+            <p>Node</p>
+            <div className="lang_info hide" id="flutter_knowledge">
+              <h1>50%</h1>
+              <p>Flutter</p>
+            </div>
+          </div>
+          <div className="techstack_box purple">
+            <FontAwesomeIcon
+              icon={faPhp}
+              color="#dfa8ff"
+              fixedWidth
+              className="fa_icon"
+            />
+            <p>PHP</p>
+            <div className="lang_info hide" id="flutter_knowledge">
+              <h1>50%</h1>
+              <p>Flutter</p>
+            </div>
+          </div>
+          <div className="techstack_box white">
+            <FontAwesomeIcon
+              icon={faWordpressSimple}
+              color="white"
+              fixedWidth
+              className="fa_icon"
+            />
+            <p>Wordpress</p>
+            <div className="lang_info hide" id="flutter_knowledge">
+              <h1>50%</h1>
+              <p>Flutter</p>
+            </div>
+          </div>
+          <div className="techstack_box red">
+            <FontAwesomeIcon
+              icon={faDatabase}
+              color="#ffa9a9"
+              fixedWidth
+              className="fa_icon"
+            />
+            <p>MS SQL</p>
+            <div className="lang_info hide" id="flutter_knowledge">
+              <h1>50%</h1>
+              <p>Flutter</p>
+            </div>
+          </div>
+          <div className="techstack_box blue">
+            <FontAwesomeIcon
+              icon={faReact}
+              color="#b0e6ff"
+              fixedWidth
+              className="fa_icon"
+            />
+            <p>React</p>
+            <div className="lang_info hide" id="flutter_knowledge">
+              <h1>50%</h1>
+              <p>Flutter</p>
+            </div>
+          </div>
+          <div className="techstack_box green">
+            <FontAwesomeIcon
+              icon={faAndroid}
+              color="#b8ffbc"
+              fixedWidth
+              className="fa_icon"
+            />
+            <p>Android</p>
+            <div className="lang_info hide" id="flutter_knowledge">
+              <h1>50%</h1>
+              <p>Flutter</p>
+            </div>
+          </div>
+          <div className="techstack_box blue">
+            <FontAwesomeIcon
+              icon={faDatabase}
+              color="lightblue"
+              fixedWidth
+              className="fa_icon"
+            />
+            <p>MySQL</p>
+            <div className="lang_info hide" id="flutter_knowledge">
+              <h1>50%</h1>
+              <p>Flutter</p>
+            </div>
+          </div>
+          <div className="techstack_box red">
+            <FontAwesomeIcon
+              icon={faLaravel}
+              color="red"
+              fixedWidth
+              className="fa_icon"
+            />
+            <p>Laravel</p>
+            <div className="lang_info hide" id="flutter_knowledge">
+              <h1>50%</h1>
+              <p>Flutter</p>
+            </div>
+          </div>
+          <div className="techstack_box white">
+            <FontAwesomeIcon
+              icon={faGithub}
+              color="white"
+              fixedWidth
+              className="fa_icon"
+            />
+            <p>Github</p>
+            <div className="lang_info hide" id="flutter_knowledge">
+              <h1>50%</h1>
+              <p>Flutter</p>
+            </div>
+          </div>
+        </div>
+      </Fade>
+    </section>
+  );
 }
